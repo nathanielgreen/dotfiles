a69feadbe5ac2b7c213cc2bc2abbfc809457087a
nathanielgreen <ngreen2303@gmail.com>
2016-08-27 13:37:00 +0100

    Added startupscripts and new wallpaper. Slight color tweak on i3

---
+++ startupscripts/foo.sh
@@ -0,0 +1,5 @@
+setxkbmap -layout gb
+#xrandr --newmode "1920x1275_60.00"  206.55  1920 2056 2264 2608  1275 1276 1279 1320  -HSync +Vsync
+#xrandr --addmode eDP1 1920x1275_60.00    
+#xrandr -s 1920x1275_60.00
+feh -s --bg-fill ~/wallpaper2.jpg
