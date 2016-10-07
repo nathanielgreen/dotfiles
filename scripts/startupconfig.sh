492773f04bd8e770498733865412cba855a75b82
nathanielgreen <ngreen2303@gmail.com>
2016-10-07 19:12:46 +0100

    Edited config for laptop and desktop feh startups

--- scripts/startupconfig.sh
+++ scripts/startupconfig.sh
@@ -1,9 +1,9 @@
 # Laptop
 setxkbmap -layout gb
-feh -s --bg-fill ~/wallpaper2.jpg
+feh -s --bg-fill ~/Documents/dotfiles/wallpapers/blackempty.png
 
 # Desktop
 #setxkbmap -layout gb
-#feh -s --no-xinerama --bg-center ~/Pictures/Firefox_wallpaper.png
+#feh -s --bg-fill ~/Documents/dotfiles/wallpapers/blackempty.png
 #xrandr --output DP-1.2.1 --mode 1920x1080 --left-of DP-1.1
 #xrandr --output DVI-D-0 --mode 1920x1080 --right-of DP-1.1
