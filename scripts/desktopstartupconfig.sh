a3d7402e0c447092db1cc1fbc714387e26ddbc23
nathanielgreen <ngreen2303@gmail.com>
2016-10-08 12:36:23 +0100

    Split startup configs for laptop and desktop

---
+++ scripts/desktopstartupconfig.sh
@@ -0,0 +1,4 @@
+setxkbmap -layout gb -option caps:super
+feh -s --bg-center ~/Documents/dotfiles/wallpapers/blackempty.png
+xrandr --output DP-1.2.1 --mode 1920x1080 --left-of DP-1.1
+xrandr --output DVI-D-0 --mode 1920x1080 --right-of DP-1.1
--- scripts/startupconfig.sh
+++
@@ -1,9 +0,0 @@
-# Laptop
-setxkbmap -layout gb
-feh -s --bg-fill ~/wallpaper2.jpg
-
-# Desktop
-#setxkbmap -layout gb
-#feh -s --no-xinerama --bg-center ~/Pictures/Firefox_wallpaper.png
-#xrandr --output DP-1.2.1 --mode 1920x1080 --left-of DP-1.1
-#xrandr --output DVI-D-0 --mode 1920x1080 --right-of DP-1.1
