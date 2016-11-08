3c3ad614e75dd159e844526d5b0d93734a544739
nathanielgreen <ngreen2303@gmail.com>
2016-11-08 17:56:20 +0000

    Updated monitor config for desktop

--- desktop/scripts/startupconfig.sh
+++ desktop/scripts/startupconfig.sh
@@ -2,5 +2,5 @@ setxkbmap -layout gb -option caps:super
 feh -s --bg-fill ~/Documents/dotfiles/wallpapers/blackempty.png
 xrandr --output DP-1.2.1 --mode 1920x1080 --left-of DP-1.1
 xrandr --output DVI-D-0 --mode 1920x1080 --right-of DP-1.1
-xrandr --output DVI-D-0 --rotate right
-xrandr --output DP-1.2.1 --rotate left
+xrandr --output DVI-D-0 --rotate normal
+xrandr --output DP-1.2.1 --rotate normal
