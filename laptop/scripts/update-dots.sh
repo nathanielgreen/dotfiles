a2ce59dfe9377312fe7c94c2f8296092a33ae28b
Nathaniel Green <ngreen2303@gmail.com>
2016-12-03 12:12:13 +0100

    Adding update-dots script

---
+++ laptop/scripts/update-dots.sh
@@ -0,0 +1,9 @@
+cp ~/.zshrc ~/Documents/dotfiles/laptop/.zshrc
+cp ~/.vimrc ~/Documents/dotfiles/laptop/.vimrc
+cp ~/.Xresources ~/Documents/dotfiles/laptop/.Xresources
+cp ~/.conkyrc ~/Documents/dotfiles/laptop/.conkyrc
+cp ~/.config/i3/config ~/Documents/dotfiles/laptop/i3/config
+cp ~/.config/dunst/dunstrc ~/Documents/dotfiles/laptop/dunstrc
+cp ~/.config/polybar/config ~/Documents/dotfiles/laptop/polybar/config
+cp ~/.config/polybar/launch.sh ~/Documents/dotfiles/laptop/polybar/launch.sh
+
