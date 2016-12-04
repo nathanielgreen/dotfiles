55270ca1dc5f226b382c45c60b2fc93776efaae2
Nathaniel Green <ngreen2303@gmail.com>
2016-12-04 13:35:54 +0100

    Updated lock

--- laptop/scripts/lock.sh
+++ laptop/scripts/lock.sh
@@ -1,11 +1,4 @@
-#!/usr/bin/env bash
-
-icon="~/Documents/dotfiles/icons/lock.png"
-tmpbg='/tmp/screen.png'
-
-(( $# )) && { icon=$1; }
-
-scrot "$tmpbg"
-convert "$tmpbg" -scale 10% -scale 1000% "$tmpbg"
-convert "$tmpbg" "$icon" -gravity center -composite -matte "$tmpbg"
-i3lock -i "$tmpbg"
+#!/bin/bash
+scrot /tmp/screen.png
+convert /tmp/screen.png -filter point -resize 10% -resize 1000% /tmp/screen.png
+i3lock -i /tmp/screen.png
