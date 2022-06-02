0b134c7f87b1f87d059c4c1cdac6a44f6d739c90
Nathaniel Green <me@nathaniel.green>
2022-06-02 14:10:43 +0100

    Add flutter tools. Fix autoread bug. Update treesitter call for segment fault bug

--- mac/lua/options.lua
+++ mac/lua/options.lua
@@ -1,15 +1,16 @@
 vim.wo.number            = true  -- Show numbers
+vim.wo.numberwidth          = 4
 vim.wo.relativenumber    = true  -- Show relative numbers
 vim.o.showtabline        = 2 	   -- Always Show Tabline
-
 vim.o.expandtab          = true  -- Convert tabs to spaces.
 vim.o.tabstop            = 2
 vim.o.softtabstop        = 2
 vim.o.shiftwidth         = 2
 vim.o.termguicolors      = true  -- Use true colors, required for some plugins
-                        
-
--- What: Open new split panes to right and bottom, which feels more natural
-vim.o.splitbelow         = true
-vim.o.splitright         = true
--- Why: Otherwise they open by default to left and the top which is mental
+vim.o.splitbelow         = true  --- Open new split panes to right and bottom, which feels more natural
+vim.o.splitright         = true  ---  Otherwise they open by default to left and the top which is mental
+vim.o.swapfile           = false -- http://robots.thoughtbot.com/post/18739402579/global-gitignore#comment-458413287
+vim.o.autowrite          = true  -- Automatically :write before running commands
+vim.o.autoread           = true  
+vim.o.backup             = false -- No autocreation of backup files
+vim.o.writebackup        = false -- No autocreation of backup files
