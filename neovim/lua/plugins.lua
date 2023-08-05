d042409304f55855d0d2fd37ea32fcfadf61a71c
Nathaniel Green <me@nathaniel.green>
2023-08-05 13:08:16 +0100

    Nvim: Switch to lazy, cmp, and neotree

--- neovim/lua/plugins.lua
+++ neovim/lua/plugins.lua
@@ -1,54 +1,63 @@
-require "paq" {
-  -- Package Manager
-  "savq/paq-nvim";                    -- Let Paq manage itself
-
+require("lazy").setup({
   -- Syntax
-  "neovim/nvim-lspconfig";       
-  "nvim-treesitter/nvim-treesitter";  -- Extra Detailed Syntax Highlighting
-  "dart-lang/dart-vim-plugin";        -- Dart Support as TS crashes randomly
-  "wuelnerdotexe/vim-astro";          -- Astro support
+  "neovim/nvim-lspconfig",       
+  "nvim-treesitter/nvim-treesitter",  -- Extra Detailed Syntax Highlighting
+  "hrsh7th/cmp-nvim-lsp",
+  "hrsh7th/cmp-buffer",
+  "hrsh7th/cmp-path",
+  "hrsh7th/cmp-cmdline",
+  "hrsh7th/nvim-cmp",
+  "hrsh7th/cmp-vsnip",
+  "hrsh7th/vim-vsnip",
+
 
   -- Themes
-  "folke/tokyonight.nvim";            -- Theme
-  "kyazdani42/nvim-web-devicons";     -- Icons for Trouble and Lualine
+  "folke/tokyonight.nvim",            -- Theme
 
   -- Navigation
-  "mcchrish/nnn.vim"; 	              -- Folder Navigation
-  "unblevable/quick-scope";           -- First Letter Highlighting
-  "nvim-lua/popup.nvim";              -- Telescope Dependency
-  "nvim-lua/plenary.nvim";            -- Telescope + Flutter Tools Dependency + Spectre
-  "nvim-telescope/telescope.nvim";
-  "ThePrimeagen/harpoon";             -- Scoped Fuzzy find
+  "unblevable/quick-scope",           -- First Letter Highlighting
+  {
+    "akinsho/bufferline.nvim",        -- Buffer Navigation
+    version = "*",
+    dependencies = "nvim-tree/nvim-web-devicons"
+  },
+  { 
+    "nvim-telescope/telescope.nvim",  -- Floating Window fuzzy finder
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-lua/popup.nvim", 
+    }
+  },
+  {
+    "nvim-neo-tree/neo-tree.nvim",    -- File tree
+    branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons",
+      "MunifTanjim/nui.nvim",
+    }
+  },
 
   -- Formatting, and linting
-  "folke/trouble.nvim";               -- Better Diagnostics
-  "mhartington/formatter.nvim";       -- Formatting
-  "windwp/nvim-spectre";              -- Project-wide Search and replace
-  "echasnovski/mini.nvim";            -- Collection of small plugins
+  "folke/trouble.nvim",               -- Better Diagnostics
+  "mhartington/formatter.nvim",       -- Formatting
+  "windwp/nvim-spectre",              -- Project-wide Search and replace
+  "echasnovski/mini.nvim",            -- Collection of small plugins
 
   -- Other
-  "github/copilot.vim";       
-  "tpope/vim-abolish";                -- For search and replace 
-  "mfussenegger/nvim-dap";            -- Debugger
-  "rcarriga/nvim-dap-ui";             -- Debugger UI
-  "stevearc/dressing.nvim";           -- Dependency for Flutter tools
-  "akinsho/flutter-tools.nvim";       -- Flutter Extras
-
-}
+  "github/copilot.vim",               -- AI Autocomplete
+  "tpope/vim-abolish",                -- For search and replace 
+  { 
+    "akinsho/flutter-tools.nvim",     -- Flutter Extras
+    lazy = false,
+    dependencies = {
+      "stevearc/dressing.nvim",
+    }
+  }
+})
 
 -- Set Leader Key
 vim.g.mapleader = " "
-
-
--- PLUGIN START: nnn
-vim.api.nvim_set_keymap("n", "-", ":NnnPicker %:p:h<CR>", { silent = true })
-vim.api.nvim_exec([[
-let g:nnn#action = {
-      \ '<c-t>': 'tab split',
-      \ '<c-s>': 'split',
-      \ '<c-v>': 'vsplit' }
-]], true)
--- PLUGIN END: nnn
 
 
 
@@ -139,34 +148,102 @@ require'lspconfig'.tsserver.setup{}
 
 --- PLUGIN START: Mini  
 require('mini.basics').setup()
-require('mini.completion').setup()
 require('mini.comment').setup()
 require('mini.indentscope').setup()
 require('mini.jump').setup()
-require('mini.cursorword').setup()
 require('mini.statusline').setup()
 --- PLUGIN END: Mini 
 
 
 
---- PLUGIN START: dap + dapui
-require("dapui").setup()
---- PLUGIN END: dap + dapui
-
-
-
---- PLUGIN START: Harpoon
---- PLUGIN END: Harpoon
-
-
-
---- PLUGIN START: vim-astro
-vim.api.nvim_exec([[
-let g:astro_typescript = 'enable'
-]], true)
---- PLUGIN END: vim-astro
-
-
 --- PLUGIN START: Flutter tools
 require("flutter-tools").setup {} -- use defaults
 --- PLUGIN END: Flutter tools
+
+
+
+--- PLUGIN START: NeoTree
+require("neo-tree").setup {}
+vim.api.nvim_set_keymap("n", "<leader>n", ":Neotree focus filesystem float<CR>", { silent = true });
+vim.api.nvim_set_keymap("n", "-", ":Neotree reveal filesystem float <CR>", { silent = true })
+-- vim.api.nvim_set_keymap("n", "-", ":Neotree focus filesystem float <CR>", { silent = true })
+--- PLUGIN END: NeoTree
+
+
+
+-- PLUGIN START: Bufferline
+-- require("bufferline").setup{}
+-- PLUGIN END: Bufferline
+
+
+
+-- PLUGIN START: Cmp
+  -- Set up nvim-cmp.
+  local cmp = require'cmp'
+
+  cmp.setup({
+    snippet = {
+      -- REQUIRED - you must specify a snippet engine
+      expand = function(args)
+        vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+        -- require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+        -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
+        -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
+      end,
+    },
+    window = {
+      -- completion = cmp.config.window.bordered(),
+      -- documentation = cmp.config.window.bordered(),
+    },
+    mapping = cmp.mapping.preset.insert({
+      ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+      ['<C-f>'] = cmp.mapping.scroll_docs(4),
+      ['<C-Space>'] = cmp.mapping.complete(),
+      ['<C-e>'] = cmp.mapping.abort(),
+      ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+    }),
+    sources = cmp.config.sources({
+      { name = 'nvim_lsp' },
+      { name = 'vsnip' }, -- For vsnip users.
+      -- { name = 'luasnip' }, -- For luasnip users.
+      -- { name = 'ultisnips' }, -- For ultisnips users.
+      -- { name = 'snippy' }, -- For snippy users.
+    }, {
+      { name = 'buffer' },
+    })
+  })
+
+  -- Set configuration for specific filetype.
+  cmp.setup.filetype('gitcommit', {
+    sources = cmp.config.sources({
+      { name = 'git' }, -- You can specify the `git` source if [you were installed it](https://github.com/petertriho/cmp-git).
+    }, {
+      { name = 'buffer' },
+    })
+  })
+
+  -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
+  cmp.setup.cmdline({ '/', '?' }, {
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = {
+      { name = 'buffer' }
+    }
+  })
+
+  -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+  cmp.setup.cmdline(':', {
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = cmp.config.sources({
+      { name = 'path' }
+    }, {
+      { name = 'cmdline' }
+    })
+  })
+
+  -- Set up lspconfig.
+  local capabilities = require('cmp_nvim_lsp').default_capabilities()
+  -- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
+  require('lspconfig')['<YOUR_LSP_SERVER>'].setup {
+    capabilities = capabilities
+  }
+-- PLUGIN END: Cmp
