9282de460a88f61d1d2835a163e96842e8c5aecd
Nathaniel Green <me@nathaniel.green>
2024-09-23 18:25:06 +0100

    Fix nvim spectre bug causing extra files
    See https://github.com/nvim-pack/nvim-spectre/issues/249 for more info

--- neovim/lua/plugins/spectre.lua
+++ neovim/lua/plugins/spectre.lua
@@ -3,5 +3,17 @@ return {
   "windwp/nvim-spectre",
   config = function() 
     vim.api.nvim_set_keymap("n", "<leader>S", ":lua require('spectre').open()<CR>", { silent = true });
+    require("spectre").setup({
+      replace_engine = {
+        ["sed"] = {
+          cmd = "sed",
+          args = {
+            "-i",
+            "",
+            "-E",
+          },
+        },
+      },
+    })
   end
 }
