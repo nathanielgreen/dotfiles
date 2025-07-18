fe49c1cc25213016207c1962aebf4db9928aa516
Nathaniel Green <me@nathaniel.green>
2025-07-18 13:57:32 +0100

    Replace formatter with conform

--- neovim/lua/plugins/nvim-lint.lua
+++ neovim/lua/plugins/nvim-lint.lua
@@ -3,15 +3,15 @@ return {
   config = function()
     local lint = require("lint")
 
-    lint.linters_by_ft = {
-      python = { "ruff" },
-    }
-
-    vim.api.nvim_create_autocmd({ "BufWritePost", "InsertLeave" }, {
-      pattern = "*",
-      callback = function()
-        lint.try_lint()
-      end,
-    })
+    -- lint.linters_by_ft = {
+    --   python = { "ruff" },
+    -- }
+    --
+    -- vim.api.nvim_create_autocmd({ "BufWritePost", "InsertLeave" }, {
+    --   pattern = "*",
+    --   callback = function()
+    --     lint.try_lint()
+    --   end,
+    -- })
   end,
 }
