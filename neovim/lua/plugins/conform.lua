7350a53404c72461db2e48386218d84e50cae978
Nathaniel Green <me@nathaniel.green>
2025-10-03 10:49:36 +0100

    Add python format config

--- neovim/lua/plugins/conform.lua
+++ neovim/lua/plugins/conform.lua
@@ -6,10 +6,17 @@ return {
       formatters_by_ft = {
         json = { 'jq' },
         dart = { 'dart_format' },
+        python = { 'black' },
       },
-      format_on_save = {
-        lsp_fallback = true,
-      },
+      format_on_save = function(bufnr)
+        -- Disable autoformat on certain filetypes
+        local ignore_filetypes = { "python" }
+        if vim.tbl_contains(ignore_filetypes, vim.bo[bufnr].filetype) then
+          return
+        end
+        -- ...additional logic...
+        return { timeout_ms = 500, lsp_format = "fallback" }
+      end,
     })
   end,
 }
